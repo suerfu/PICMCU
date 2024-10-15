ff3bb31cc025d8a2f4ac01cec88f44402d7e417c
Suerfu Burkhant <suerfu@post.kek.jp>
2024-10-15 14:27:16 +0900

    added comments about essential functions in I2C; reorganized directory structure in main.

--- PIC16F1788/functions/mssp.h
+++ PIC16F1788/functions/mssp.h
@@ -25,7 +25,8 @@ void ConfigI2C(){
 
     // Configure mode for I2C
     //
-    SSP1STAT = 0x80;
+    SSP1STAT = 0x80;    // 0x00 also works (2024-10-15)
+    
     //SSPCON1bits.SSPM = 0x8;
     //SSPCON1bits.SSPEN = 1;
     SSP1CON1 = 0x28;
@@ -34,13 +35,15 @@ void ConfigI2C(){
     
     // Configure baud rate
     //
-    SSP1ADD = 0x4f; // for 100 kHz operation
-    //SSP1ADD = 0x13; // 400 kHz operation
+    //SSP1ADD = 0x4f; // for 100 kHz operation
+    SSP1ADD = 0x13; // 400 kHz operation
+        // 400 kHz also works (2024-10-15)
 }
 
 
 void I2C_Master_Wait(){
     while( (SSP1STAT & 0x01) || (SSP1CON2 & 0x1F) );
+        // it seems this wait function is not very essential to the function
 }
 
 
@@ -51,12 +54,7 @@ void I2C_Master_Start(){
     PIR1bits.SSP1IF = 0;
         // SEN is cleared by hardware automatically
         // SSP1IF needs to be cleared manually
-    /*
-    if( PIR1bits.SSP1IF==1 ){
-        printf("Flag is on\n\r");
-        PIR1bits.SSP1IF = 0;
-    }
-     */
+        // Both the two above lines are essential (2024-10-15)
 }
 
 
@@ -71,58 +69,12 @@ void I2C_Master_Stop(){
 
 
 char I2C_Master_Write( char d ){
-    //while( SSP1STATbits.BF );
     I2C_Master_Wait();
     SSPBUF = d;
-    // wait for interrupt flag
     while ( !PIR1bits.SSP1IF ){;}
+        // wait for interrupt flag
+        // this wait for finish flag is also essential for proper function (2024-10-15)
     return SSPCON2bits.ACKSTAT;
-}
-
-
-
-/*
-int I2CWaitAck(){
-
-    for( char i=0; i<255; i++){
-        if( SSPCON2bits.ACKSTAT==0 ){
-            return 0;
-        }
-    }
-    return -1;  // slave not present/data not received
-}
-*/
-
-int I2CWrite( char addr, char* data, char n){
-    
-    I2C_Master_Start();
-    
-    //while( SSPCON2bits.SEN==1 );
-    //I2CIFPoll();
-
-    // write address and write-bit
-    I2C_Master_Wait();
-    SSP1BUF = ( addr & 0xfe );
-    //while( SSPSTATbits.BF == 1 ){}
-
-    // check ack status
-    //int ack = I2CWaitAck();
-    //if( ack<0 ){
-    //    return -1;  // slave not present
-    //}
-    
-    // write data for transmission
-    for( int i=0; i<n; i++){
-        I2C_Master_Wait();
-        SSP1BUF = data[i];
-        /*while( SSPSTATbits.BF == 1 ){}
-        ack = I2CWaitAck();
-        if( ack<0 ){
-            return -2;  // slave present but data not received
-        }*/
-    }
-
-    return 0;
 }
 
 
